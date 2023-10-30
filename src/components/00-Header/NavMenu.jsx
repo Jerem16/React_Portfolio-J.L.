034d2530808da30cc98b99f448a732bf06373847
Jeremy_lem <jeremy.lemaignent@gmail.com>
2023-10-30 06:53:55 +0100

    v22

--- src/components/00-Header/NavMenu.jsx
+++ src/components/00-Header/NavMenu.jsx
@@ -1,32 +1,38 @@
 import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
-// import shortid from "shortid";
+// import { NavLink, useLocation } from "react-router-dom";
+import { Link, animateScroll as scroll } from "react-scroll";
+
 import { nanoid } from "nanoid";
 import LanguageSelector from "./01-LanguageSelector";
 import ThemeSelector from "./ThemeSelector/ThemeSelector";
 
 function NavMenu({ navLinks, handleClick, language, id }) {
-    const location = useLocation();
+    // const location = useLocation();
 
     return (
         <ul className="nav" id={id}>
             {navLinks.map((link) => (
                 <li onClick={handleClick} className="head-btn" key={nanoid()}>
-                    <i
-                        className={`icon ${ 
-                            location.pathname === link.to ? "active-link " : ""
-                        }
-                        
-                        ${link.icon}`}
-                    ></i>
-                    <NavLink
+                    <Link
                         to={link.to}
-                        className={
-                            location.pathname === link.to ? "active-link" : ""
-                        }
+                        activeClass="active-link"
+                        spy={true}
+                        smooth={true}
+                        offset={-60}
+                        duration={500}
+                    >
+                        <i className={`icon ${link.icon}`}></i>
+                    </Link>
+                    <Link
+                        to={link.to}
+                        activeClass="active-link"
+                        spy={true}
+                        smooth={true}
+                        offset={-60}
+                        duration={500}
                     >
                         {link.text}
-                    </NavLink>
+                    </Link>
                 </li>
             ))}
             <LanguageSelector text={language} />
